375daf567d3c8206d66c3ba8d67b40b652d1c166
Jaylen Wilson <Jaylenaverywilson@gmail.com>
2022-02-12 12:12:36 -0500

    first commit push to heroku

---
+++ src/helpers/environment.tsx
@@ -0,0 +1,13 @@
+let APIURL = "";
+
+switch (window.location.hostname) {
+
+    case 'localhost' || '127.0.0.1':
+        APIURL = 'http://localhost:5000';
+        break;
+
+    case 'https://jw-prody-client.herokuapp.com/':
+        APIURL = 'https://jw-prody-client.herokuapp.com/';
+}
+
+export default APIURL
